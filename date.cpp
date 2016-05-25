dfccb7c1d63cc8e57185f77e9ab8bc8cf20df34a
mbortnowski <bortnowskim@gmail.com>
2016-05-25 15:40:09 +0200

    Update date.cpp

--- date.cpp
+++ date.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 #include <ctime>
 
+using namespace std;
+
 int main()
 {
-    struct std::tm a = {0,0,0,24,5,104}; /* June 24, 2004 */
-    struct std::tm b = {0,0,0,5,6,104}; /* July 5, 2004 */
-    std::time_t x = std::mktime(&a);
-    std::time_t y = std::mktime(&b);
-    if ( x != (std::time_t)(-1) && y != (std::time_t)(-1) )
+    struct tm a = {0,0,0,24,5,104}; /* June 24, 2004 */
+    struct tm b = {0,0,0,5,6,104}; /* July 5, 2004 */
+    time_t x = mktime(&a);
+    time_t y = mktime(&b);
+    if ( x != (time_t)(-1) && y != (time_t)(-1) )
     {
-        double difference = std::difftime(y, x) / (60 * 60 * 24);
-        std::cout << std::ctime(&x);
-        std::cout << std::ctime(&y);
-        std::cout << "difference = " << difference << " days" << std::endl;
+        double difference = difftime(y, x) / (60 * 60 * 24);
+        cout << ctime(&x);
+        cout << ctime(&y);
+        cout << "difference = " << difference << " days" << endl;
     }
     return 0;
 }
