0bfd6256ac7f73e43f8ffaa8f1cbbdc3cd4f35c3
mbortnowski <bortnowskim@gmail.com>
2016-06-08 17:05:29 +0200

    Add files via upload

--- src/ConsoleApplication1.cpp
+++ src/ConsoleApplication1.cpp
@@ -11,11 +11,49 @@ int main()
 
 
 	vector <Klient *> ListaKlientow;
-	vector <Pokoj> ListaPokoi;
+	vector <Pokoj *> ListaPokoi;
 
 	vector <Recepcjonista> ListaPracownikow;
 
+	Pokoj * P;
 	Klient * K;
+	
+	//////////////////////////////POKOJE TESTOWE
+	for (unsigned int K = 0; K < 30; K++)
+	{
+		P = new Pokoj(K,(K%3)+1,"Pokoj",true);
+		ListaPokoi.push_back(P);
+	}
+	for (unsigned int ik = 0; ik < ListaPokoi.size(); ik++)
+	{
+		ListaPokoi[ik]->get();
+	}
+	system("pause");
+
+	
+	for (unsigned int j = 0;j < 1; j++)
+	{
+		K = new Klient();
+		ListaKlientow.push_back(K);
+	}
+
+	system("pause");
+	system("cls");
+
+	for (unsigned int ij = 0; ij < ListaKlientow.size(); ij++)
+	{
+		ListaKlientow[ij]->get();
+	}
+
+	system("pause");
+
+
+
+
+
+
+
+	//MENU G£ÓWNE
 
 	do{
 
