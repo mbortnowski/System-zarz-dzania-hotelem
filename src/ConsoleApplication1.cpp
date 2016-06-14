511a5f1c5d8aaf53c253f16c7fa32f70ab2efdfb
mbortnowski <bortnowskim@gmail.com>
2016-06-15 01:17:17 +0200

    Add files via upload

--- src/ConsoleApplication1.cpp
+++ src/ConsoleApplication1.cpp
@@ -7,7 +7,7 @@ int main()
 {
 	srand(time(NULL)); //TESTOWE
 
-	int ster = 1, cid;
+	int ster = 1; unsigned int cid;
 	string h;
 
 
@@ -24,30 +24,11 @@ int main()
 	//LOSUJ 1-30 (rand() % 30)+1;
 
 
-	//test.Przegladaj_pokoje(ListaPokoi);
-	//test.Przegladaj_klientow(ListaKlientow);
-	//test.Znajdz_klienta(ListaKlientow);
-
 	cout << "*************" << endl;
 	cout << "*** HOTEL ***" << endl;
 	cout << "*************" << endl << endl;
 	system("pause");
 
-	//Rezerwacja * R;
-	
-	//R = new Rezerwacja();
-	//R->Dodaj_pokoje(ListaPokoi);
-	//R->Wyswietl_pokoje();
-	//R->Wyswietl_pokoje();
-	//R->get();
-
-	//K = new Klient();
-	//system("cls");
-	//K->Dodaj_rezerwacje();
-	//system("cls");
-	//K->Przegladaj_rezerwacje();
-	//K->Potwierdz_rezerwacje(1);
-	//K->Przegladaj_rezerwacje();
 
 	//MENU G£ÓWNE
 
@@ -58,22 +39,23 @@ int main()
 
 		cout << "1. Dodaj klienta" << endl;
 		cout << "2. Dodaj pokoj" << endl;
-		cout << "3. Wyswietl pokoje" << endl;
-		cout << "4. Wyswietl klientiow" << endl;
-		cout << "5. Dodaj 30 testowych pokoi" << endl;
-		cout << "6. Dodaj 30 testowych klientow" << endl;
-		cout << "7. Dodaj klient + rezerwacja" << endl;
-		cout << "8. Znajdz klienta (ID)" << endl;
-		cout << "9. Znajdz klienta (imie, nazwisko)" << endl;
-		cout << "10. Przeglaj rezerwacje klienta" << endl;
-		cout << "11. Potwierdz rezerwacje klienta" << endl;
-		cout << "12. Dodaj rezerwacje do klienta" << endl;
-		cout << "13. Logowanie" << endl;
-		cout << "14. Wyswietl rezerwacje" << endl;
-		cout << "15. Dodaj rezerwacje testowe (wymagane: opcja 5 i 6 oraz brak innych rezerwacji)" << endl;
-		cout << "16. Znajdz pokoj (ID)" << endl;
-		cout << "17. Wyswietl pokoje na pietrze" << endl;
-		cout << "18. Zamelduj goscia (ID)" << endl;
+		cout << "3. Dodaj rezerwacje do klienta" << endl;
+		cout << "4. Dodaj klient + rezerwacja" << endl;
+		cout << "5. Wyswietl pokoje" << endl;
+		cout << "6. Wyswietl klientow" << endl;
+		cout << "7. Wyswietl rezerwacje" << endl;
+		cout << "8. Przeglaj rezerwacje klienta" << endl;
+		cout << "9. Wyswietl pokoje na pietrze" << endl;
+		cout << "10. Znajdz klienta (ID)" << endl;
+		cout << "11. Znajdz klienta (imie, nazwisko)" << endl;
+		cout << "12. Znajdz pokoj (ID)" << endl;
+		cout << "13. Znajdz rezerwacje(ID)" << endl;
+		cout << "14. Zamelduj goscia (ID)" << endl;
+		cout << "15. Potwierdz rezerwacje goscia" << endl;
+		cout << "16. Logowanie" << endl;
+		cout << "17. Dodaj 30 testowych pokoi" << endl;
+		cout << "18. Dodaj 30 testowych klientow" << endl;
+		cout << "19. Dodaj rezerwacje testowe (wymagane: opcja 17 i 18 oraz brak innych rezerwacji)" << endl;
 		cout << "0. Zakoncz" << endl;
 
 
@@ -95,46 +77,32 @@ int main()
 			break;
 
 		case 3:
-			test.Przegladaj_pokoje(ListaPokoi);
+			cout << "Podaj ID klienta: ";
+			cin >> cid;
+			if (cid + 1 > ListaKlientow.size()) { break; }
+			ListaKlientow[cid]->Dodaj_rezerwacje();
 			break;
 
 		case 4:
-			test.Przegladaj_klientow(ListaKlientow);
-			break;
-
-		case 5:
-			for (unsigned int K = 0; K < 30; K++)
-			{
-				P = new Pokoj(K, (K % 3) + 1, "Pokoj", true);
-				ListaPokoi.push_back(P);
-			}
-			break;
-
-		case 6:
-			for (unsigned int g = 0; g < 30; g++)
-			{
-				struct tm DZ = { 0,0,0,0,0,0 };
-				K = new Klient("Test", "Testowy", "ul testowa", 1111111 * g, 0, DZ, "Haslo", "test sp zoo");
-				ListaKlientow.push_back(K);
-			}
-			break;
-
-		case 7:
 			K = new Klient();
 			K->Dodaj_rezerwacje();
 			K->Przegladaj_rezerwacje();
 			break;
 
+		case 5:
+			test.Przegladaj_pokoje(ListaPokoi);
+			break;
+
+		case 6:
+			test.Przegladaj_klientow(ListaKlientow);
+			break;
+
+		case 7:
+			test.Przegladaj_rezerwacje(ListaKlientow);
+			system("pause");
+			break;
+
 		case 8:
-			test.Znajdz_klienta(ListaKlientow);
-			break;
-
-
-		case 9:
-			test.Znajdz_klienta2(ListaKlientow);
-			break;
-
-		case 10:
 			cout << "Podaj ID klienta: ";
 			cin >> cid;
 			if (cid + 1 > ListaKlientow.size()) { break; }
@@ -142,20 +110,26 @@ int main()
 			system("pause");
 			break;
 
-		case 11:
-			cout << "Podaj ID klienta: ";
+		case 9:
+			cout << "Podaj pietro: ";
 			cin >> cid;
-			int rid;
-			if (cid + 1 > ListaKlientow.size()) { break; }
-			cout << "Podaj ID rezerwacji: ";
-			cin >> rid;
-			ListaKlientow[cid]->Potwierdz_rezerwacje(rid);
+			if (cid + 1 > ListaPokoi.size()) { break; }
+			test.Znajdz_pokoj2(ListaPokoi, cid);
 			break;
+
+		case 10:
+			test.Znajdz_klienta(ListaKlientow);
+			break;
+
+		case 11:
+			test.Znajdz_klienta2(ListaKlientow);
+			break;
+
 		case 12:
-			cout << "Podaj ID klienta: ";
+			cout << "Podaj ID pokoju: ";
 			cin >> cid;
-			if (cid + 1 > ListaKlientow.size()) { break; }
-			ListaKlientow[cid]->Dodaj_rezerwacje();
+			if (cid + 1 > ListaPokoi.size()) { break; }
+			test.Znajdz_pokoj(ListaPokoi, cid);
 			break;
 
 		case 13:
@@ -164,18 +138,55 @@ int main()
 			if (cid + 1 > ListaKlientow.size()) { break; }
 			cout << "Podaj haslo: ";
 			cin >> h;
-			ListaKlientow[cid]->Logowanie(cid,h);
+			ListaKlientow[cid]->Logowanie(cid, h);
 			break;
 
 		case 14:
-			test.Przegladaj_rezerwacje(ListaKlientow);
-			system("pause");
+			cout << "Podaj ID goscia: ";
+			cin >> cid;
+			if (cid + 1 > ListaKlientow.size()) { break; }
+			ListaKlientow[cid]->zamelduj();
 			break;
 
 		case 15:
+			cout << "Podaj ID klienta: ";
+			cin >> cid;
+			int rid;
+			if (cid + 1 > ListaKlientow.size()) { break; }
+			cout << "Podaj ID rezerwacji: ";
+			cin >> rid;
+			ListaKlientow[cid]->Potwierdz_rezerwacje(rid);
+			break;
+
+		case 16:
+			cout << "Podaj ID rezerwacji: ";
+			cin >> cid;
+			if (ListaKlientow.size() == 0) { break; }
+			test.Znajdz_rezerwacje(ListaKlientow, cid);
+			system("pause");
+			break;
+
+		case 17:
+			for (unsigned int K = 0; K < 30; K++)
+			{
+				P = new Pokoj(K, (K % 3) + 1, "Pokoj", true);
+				ListaPokoi.push_back(P);
+			}
+			break;
+
+		case 18:
+			for (unsigned int g = 0; g < 30; g++)
+			{
+				struct tm DZ = { 0,0,0,0,0,0 };
+				K = new Klient("Test", "Testowy", "ul testowa", 1111111 * g, 0, DZ, "Haslo", "test sp zoo");
+				ListaKlientow.push_back(K);
+			}
+			break;
+			
+		case 19:
 			if (ListaKlientow.size()<5) { break; }
 
-			for (unsigned int hx = 1; hx < 3; hx++)
+			for (unsigned int hx = 1; hx < 6; hx++)
 			{
 				struct tm DR = { 0,0,0,1,0,2016-1900 };
 				struct tm DZ = { 0,0,0,10,0,2016-1900 };
@@ -187,27 +198,6 @@ int main()
 
 			}
 			system("pause");
-			break;
-
-		case 16:
-			cout << "Podaj ID pokoju: ";
-			cin >> cid;
-			if (cid + 1 > ListaPokoi.size()) { break; }
-			test.Znajdz_pokoj(ListaPokoi,cid);
-			break;
-
-		case 17:
-			cout << "Podaj pietro: ";
-			cin >> cid;
-			if (cid + 1 > ListaPokoi.size()) { break; }
-			test.Znajdz_pokoj2(ListaPokoi, cid);
-			break;
-
-		case 18:
-			cout << "Podaj ID goscia: ";
-			cin >> cid;
-			if (cid + 1 > ListaKlientow.size()) { break; }
-			ListaKlientow[cid]->zamelduj();
 			break;
 
 		case 0:
